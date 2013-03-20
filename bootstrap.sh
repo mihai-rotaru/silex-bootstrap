859e2a8ab2d8f6d7549634670f763613c74b239b
Mihai Rotaru <mihai_rotaru@lavabit.com>
2013-03-20 11:09:23 +0000

    enable mod_rewrite on bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
+# /bin/bash
 
 sudo apt-get update
 sudo apt-get install -y apache2
 sudo apt-get install -y php5
 sudo /etc/init.d/apache2 restart
+sudo a2enmod rewrite
 sudo ln -s /vagrant/ /var/www/silex-bootstrap

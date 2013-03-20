b256242cc00c53e11e31cfe4e9c4fd228381d9b1
Mihai Rotaru <mihai_rotaru@lavabit.com>
2013-03-20 12:21:44 +0000

    Apache config to enable AllowOverrides

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,15 @@
 sudo apt-get update
 sudo apt-get install -y apache2
 sudo apt-get install -y php5
+
+# enable mod_rewrite
 sudo a2enmod rewrite
-sudo /etc/init.d/apache2 restart
+
+# enable and config mod_rewrite for silex-bootstrap
+sudo cp /vagrant/default /etc/apache2/sites-available/
+
+# create symbolic link so the folder is served by VM's Apache 
 sudo ln -v -s /vagrant/ /var/www/silex-bootstrap
+
+# restart Apache
+sudo /etc/init.d/apache2 restart
